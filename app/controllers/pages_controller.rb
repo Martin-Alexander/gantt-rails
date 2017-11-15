1fb5e6cd2a5e0a8dfd4876cb3dd4ec1e218306f3
Martin Giannakopoulos <martingianna@gmail.com>
2017-11-15 14:04:38 -0500

    adding comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,28 @@
 class PagesController < ApplicationController
+  # !!! You'll probably want to add these methods to your tasks controller and give them different names
+
+  # Renders the demo page with the Gantt chart
   def main
+    # Turns the list of tasks into a list of hashes
+
+    # !!! You'll probably want to filter by user_id
     @tasks = JSON.parse(Task.all.order(:id).to_json)
   end
 
+  # POST /save
+  # Recieves a list of tasks from the hidden form
   def save
+
+    # Creates an array of hashes containing data about each task
     tasks = JSON.parse(params[:task_list]["other"])["tasks"]
+
+    # Iterating through all the task hashes
     tasks.each do |task_params|
+
+      # Look up the correct task from the database
       task = Task.find(task_params["id"])
+
+      # Update it with new data
       task.update!({
         name: task_params["name"],
         start: Date.parse(task_params["start"]),
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#main"
 
+  # New route
   post "/save", to: "pages#save", as: "save_tasks"
 end
